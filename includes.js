f80fe308d4006733397fa32c0c500ede4a781a36
rakibul <rakibul1810130@gmail.com>
2025-06-12 12:04:18 +0600

    Check array or not

--- includes.js
+++ includes.js
@@ -3,6 +3,13 @@
 // Create an array of books containing different book.
 // Use the includes method to check if the array contains a javascript book.
 // Print a message to the console indicating whether the element is present in the array or not.
-const book=['Bangla','English','physics','Chemistry','Math'];
+const book = ['Bangla', 'English', 'physics', 'Chemistry', 'Math'];
 console.log(book);
+console.log(book.includes('math'));
+if (book.includes('Math')) {
+    console.log("The book is present here");
+} else {
+    console.log("Not Present");
+}
+
 
