e5231584afaf9bde72e28f63415906cea9fcf773
rakibul <rakibul1810130@gmail.com>
2025-06-12 12:11:13 +0600

    concate two array

---
+++ combine.js
@@ -0,0 +1,14 @@
+// 5. Combining Arrays
+// Instructions:
+// Create two arrays of your choice.
+// Use the concat method to combine the two arrays into a new array.
+// Print both the original arrays and the combined array using console.log().
+
+const arry1=[1,2,3,'bangla','english'];
+const arra2=[4,5,6,'math','phy'];
+ 
+console.log(arry1);
+console.log(arra2);
+
+console.log(arry1.concat(arra2));
+
